f622b7bdf4145103beffa403dda8b71c62dc9984
ashadulislam6156rs <ashadulislam6156rs@gamil.com>
2026-02-12 23:46:47 +0600

    utilitis base functions added

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,12 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 
 const app = express();
+
+
+app.use(cors({origin:process.env.ORIGIN_PORT, credentials: true}));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({extended: true, limit:"1mb"}));
+// app.use(express.static("public"));
 
 export { app };
